37a2a92649f9bda7bb9d5fb299298d93e59e07c6
Seung jun <poodle2405@gmail.com>
2021-03-26 22:19:10 +0900

    UPDATE class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
-const person = {
-  name: "seung jun",
-  age: 18,
-  gender: "male",
-};
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender?: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const park = new Human("Seung jun", 18, "male");
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(park));
 
 export {};
